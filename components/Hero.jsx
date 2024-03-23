ce7f130f6e4736b05d67c1c4e1128307f6a2fdb2
sharbas <mohammedsharbas32@gmail.com>
2024-03-24 04:15:16 +0530

    10th commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,7 +22,7 @@ function Hero() {
 
 <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'> 
 <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>
-Web Developer
+MERN STACK DEVELOPER
 </div>
 <h1 className='h1 mb-4'>Hello, my name is Sharbas Mohammed</h1>
 <p className='subtitle max-w-[490px] mx-auto xl:mx-0 '>Brief description with insights into myself, my vacational journey, and what i engage in professionally</p>
