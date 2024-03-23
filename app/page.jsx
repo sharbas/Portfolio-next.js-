0f2877a6b7045c8e1ad4bebe05aaa4898c075c6b
sharbas <mohammedsharbas32@gmail.com>
2024-03-24 02:42:27 +0530

    3rd commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Cta from "@/components/Cta";
 import Hero from "@/components/Hero";
 import Reviews from "@/components/Reviews";
 import Services from "@/components/Services";
-import Work from "@/components/work";
+import Work from "@/components/Work";
 // import Image from "next/image";
 
 export default function Home() {
